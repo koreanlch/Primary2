821fb78ac6368805ef4adddce45f9d9199c97c48
Park, Jonghyeon <me@ho9.me>
2019-08-02 22:42:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let support = [
     {display : 's=vt', id : ['s', 'v', 't'], calc : 0},
-    {display : '2as=v^2-v^2.', id : ['w', 'a', 's', 'v', 'v.'], calc : 2},
-    {display : 's=vt+1/2at', id : ['s', 'v', 't', 'a', 't'], calc : 3},
+    {display : '2as=v^2-v\'^2', id : ['a', 's', 'v', 'v\''], calc : 2},
+    {display : 's=vt+1/2at^2', id : ['s', 'v', 't', 'a', 't'], calc : 3},
     {display : '평균 속도', id : ['v평', 'v0', 'v'], calc : 4},
     {display : 'F=ma', id : ['F', 'm', 'a'], calc : 0},
     {display : 'I=Ft', id : ['I', 'F', 't'], calc : 0},
@@ -41,31 +41,55 @@ function inputPcalc (type, index) {  //id, type
         result.push(support[index]['id'][req[0]])
         if (type === 0) {
             if (req[0] === 0) {
-                result.push(Number(document.getElementById('inp1').value) * Number(document.getElementById('inp2').value))
+                result.push(v(1) * v(2))
             } else if (req[0] === 1) {
-                result.push(Number(document.getElementById('inp0').value) / Number(document.getElementById('inp2').value))
+                result.push(v(0) / v(2))
             } else {
-                result.push(Number(document.getElementById('inp0').value) / Number(document.getElementById('inp1').value))
+                result.push(v(0) / v(1))
             }
         } else if (type === 1) {
             if (req[0] === 0) {
-                result.push((Number(document.getElementById('inp1').value) * Number(document.getElementById('inp2').value)).toString() + 'g')
+                result.push((v(1) * v(2)).toString() + 'g')
             } else if (req[0] === 1) {
-                result.push(Number(document.getElementById('inp0').value) / Number(document.getElementById('inp2').value))
+                result.push(v(0) / v(2))
             } else {
-                result.push(Number(document.getElementById('inp0').value) / Number(document.getElementById('inp1').value))
+                result.push(v(0) / v(1))
             }
         } else if (type === 2) {
+            if (req[0] === 0) {
+                result.push((v(2) ** 2 - v(3)**2) / (2 * v(1)))
+            } else if (req[0] === 1) {
+                result.push((v(2) ** 2 - v(3)**2) / (2 * v(0)))
+            } else if (req[0] === 2) {
+                result.push((2 * v(0) * v(1) + v(3)) ** (1/2))
+            } else {
+                calced = (-1 * (2 * v(0) * v(1) - v(2))) ** (1/2)
+                if (isNaN(calced)) {
+                    result.push('존재하지 않음')
+                } else {
+                    result.push(calced)
+                }
+            }
         } else if (type === 3) {
+            if (req[0] === 0) {
+                result.push(((v(1) * v(2)) + (v(3) * v(4)) / 2))
+            } else if (req[0] === 1) {
+                result.push((v(0) - (v(3) * v(4))/2)) / v(2)
+            } else if (req[0] === 2) {
+                result.push((v(0) - (v(3) * v(4))/2)) / v(3)
+            } else if (req[0] === 3) {
+                result.push((2 * (v(0) - v(1) * v(2))) / v(4)**2)
+            } else {
+                result.push((2 * (v(0) - v(1) * v(2))) / v(3)**1/2)
+            }
         } else if (type === 4) {
             if (req[0] === 0) {
-                result.push((Number(document.getElementById('inp1').value) + Number(document.getElementById('inp2').value))/2)
+                result.push((v(1) + v(2))/2)
             } else if (req[0] === 1) {
-                result.push(2*(Number(document.getElementById('inp0').value) - Number(document.getElementById('inp2').value)))
+                result.push(2*(v(0) - v(2)))
             } else {
-                result.push(2*(Number(document.getElementById('inp0').value) - Number(document.getElementById('inp1').value)))
+                result.push(2*(v(0) - v(1)))
             }
-        } else {
         }
         document.getElementById('tag').innerHTML = renderResultTag(result[0], result[1], 'success')
     }
@@ -73,4 +97,8 @@ function inputPcalc (type, index) {  //id, type
 
 function renderResultTag (first, second, color) {
     return '<div class="control"><div class="tags has-addons"><span class="tag">' + first + '</span><span class="tag is-' + color + '">' + second + '</span></div></div>'
+}
+
+function v (id) {
+    return Number(document.getElementById('inp'+id).value)
 }
